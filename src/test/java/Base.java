065c6b418adf2bc9d1174fe588b890d1db4f4d3e
hall2020 <halilarbaz@gmail.com>
2024-02-17 10:24:14 -0500

    the basic working solar system

---
+++ src/test/java/Base.java
@@ -0,0 +1,13 @@
+public class Base {
+    String Sun;
+    String Moon;
+    String Planets;
+    String Stars;
+    public static void main(String[] args) {
+        
+        Base solar = new Base();
+        solar.Stars = "100";
+        solar.Moon = "4";
+        solar.Planets = "2";
+    }
+}
