39b7656ed4444f9229113c99321654b33440b32c
hall2020 <halilarbaz@gmail.com>
2024-02-17 11:15:23 -0500

    i have worked on featuresunplease review and merge after review

---
+++ src/test/java/FeatureSun.java
@@ -0,0 +1,10 @@
+public class FeatureSun {
+
+    String Color;
+    String Size;
+    String temp;
+
+    public static void main(String[] args) {
+        
+    }
+}
--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -1,3 +1,15 @@
 public class SolarSystem {
+    public static void main(String[] args) {
+
+        Base solar = new Base();
+        solar.Stars="100";
+        solar.Moon="4";
+        solar.Planets="2";
+
+        FeatureSun xros= new FeatureSun();
+        xros.Color="pink";
+        xros.temp="23243243243234k";
+        xros.Size="234243432432432424miles";
+    }
 
 }
